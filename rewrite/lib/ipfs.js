bc5488576e1d745f1554c35bd708110e2f1d5038
VoR0220 <catalanor0220@gmail.com>
2015-11-21 20:19:59 -0600

    Update ipfs.js

--- rewrite/lib/ipfs.js
+++ rewrite/lib/ipfs.js
@@ -1,10 +1,28 @@
-var deasync = require("deasync");
+var fs = require('fs');
+var ipfsAPI = require('ipfs-api');
+var deasync = require('deasync');
+
+// connect to ipfs daemon API server
+var ipfs = ipfsAPI('localhost', '5001') // leaving out the arguments will default to these values
 
 module.exports = class IPFS {
     getSync(hash, destination_path, recursive) {
+        var file;
         if( recursive !== undefined ) {
+        
+        }
+
+        file = ipfs.cat(hash, function(err, res) {
+            if(err || !res) return console.error(err)
+
+            return fs.writeFile(destination_path, res)
+        });
+
+        while(file === undefined) {
+            deasync.runLoopOnce();
         }
     }
+
     addSync(hash, source_path, recursive) {
     }
 }
